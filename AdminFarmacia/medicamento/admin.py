ff6b79bdf020a49bfd98214618b46234f6f89738
= <=>
2026-06-10 12:34:44 -0300

    Falta lo d MACa

--- AdminFarmacia/medicamento/admin.py
+++ AdminFarmacia/medicamento/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Estado, Nivel_Riesgo, Medicamento
+
+admin.site.register(Estado)
+admin.site.register(Nivel_Riesgo)
+admin.site.register(Medicamento)
 
 # Register your models here.
--- AdminFarmacia/medicamento/urls.py
+++ AdminFarmacia/medicamento/urls.py
@@ -13,17 +13,3 @@ urlpatterns = [
     path('modificaciones_medicamento/<int:id_medicamento>/', views.modificaciones_medicamento, name='modificaciones_medicamento'),
      
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-def lista_medicamento(request):
-    # Aquí puedes buscar los medicamentos de la base de datos más adelante
-    return render(request, 'medicamento/lista_medicamento.html')
-
-def alta_medicamento(request):
-    return render(request, 'medicamento/alta_medicamento.html')
-
-def eliminacion_medicamento(request, id_medicamento):
-    # Lógica para eliminar aquí
-    return redirect('lista_medicamento')
-
-def modificaciones_medicamento(request, id_medicamento):
-    return render(request, 'medicamento/modificaciones_medicamento.html')
